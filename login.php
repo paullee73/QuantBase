5f00ee6a2aba521d490ca5b643f03b44e5cc4012
Arpit Rupakhetee <ar9cd@virginia.edu>
2018-05-01 19:56:56 -0400

    finalized.

--- login.php
+++ login.php
@@ -1,140 +1,53 @@
-<?php 
-session_start();
+<?php
+	$postdata = file_get_contents("php://input");
+	$request = json_decode($postdata);
+	
+	$inputName = $request->username;
+    $inputPass = $request->password;
 ?>
 
-<!DOCTYPE html>
-<html>
 
-<head>
-    <link rel="stylesheet" href="index.css" type="text/css">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
-    <title>Quantbase</title>
-</head>
-
-<body>
-    <!-- navigation bar -->
-    <header>
-        <nav class="navbar navbar-inverse navbar-fixed-top">
-            <div class="container">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"
-                        aria-expanded="false">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="index.php">QuantBase</a>
-                </div>
-                <!-- elements in navbar to collapse into button -->
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <ul class="nav navbar-nav">
-                        <li>
-                            <a href="exchange.php">Exchange</a>
-                        </li>
-                        <li>
-                            <a href="portfolio.php">Portfolio</a>
-                        </li>
-                    </ul>
-                    <ul class="nav navbar-nav navbar-right" id="top-right">
-                        <li>
-                            <a href="signup.php">
-                                <span class="glyphicon glyphicon-user"></span> Sign Up</a>
-                        </li>
-                        <li>
-                            <a href="login.php">
-                                <span class="glyphicon glyphicon-log-in"></span> Login</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    </header>
-
-        <!-- Sign up form -->
-        <div class="container" id="signup-form" style="padding-top: 120px">
-        <div class="row">
-            <div class="col-md-2">
-            <!-- take 2 user inputs, email and password, post them to $_SERVER -->
-            </div>
-            <div class="col-md-8" id="form">
-                <form class="form-group" action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
-                    <label for="name">Email Address:</label>
-                    <input class="form-control" name="email" 
-                    value="<?php if (isset($_POST['email'])) echo $_POST['email']; ?>">
-                
-                <form class="form-group" action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
-                    <label for="email">Password:</label>
-                    <input class="form-control" name="password" type="password"
-                    value="<?php if (isset($_POST['password'])) echo $_POST['password']; ?>">
-                    <input type="submit"/>
-                </form>
-            </div>
-        </div>
-        <div class="col-md-2">
-
-        </div>
-    </div>
 
 <?php
-//definition of user
-class user{
-    public $name;
-    public $email;
-    public $password;
-    public $stocks;
-    public function __construct($name, $email, $password, $stocks){
-        $this->name = $name;
-        $this->email= $email;
-        $this->password = $password;
-        $this->stocks = $stocks;
-    }
-}
-$feedback = NULL;
-// $bar = new user("Paul", "asdf@asdf", "rafara", array("GOOGL", "MSF"));
-// echo $bar->stocks[0];
 
-//whenever user tries to login
-if ($_SERVER['REQUEST_METHOD'] == 'POST')
-{
-    //if any fields are empty
-   if (empty($_POST['email']) || empty($_POST['password']))
-      echo "<center><font color='red'><i>Please fill out all fields</i></font> <br /></center>";
-   else 
-   {   	
-        $email = trim($_POST['email']);      // trim() remove leading space
-        $password = trim($_POST['password']);
-        $match = 0;
-        //if there is a match in $_SESSION, then sign in
-        foreach($_SESSION as $key => $value){
-            if($key == $email && $value->password == $password){
-                $match++;
-            }
-        }
-        if($match > 0){
-            echo "<center><font color='green'><i>You are signed in!</i></font></center>";
-        }
-        else{
-            //alert user there is no match
-            echo "<center><font color='red'><i>Username/password is not correct</i></font></center>";
-        }
-   }      
-}
-?>
+                $user = 'root';
+                $pass = '';
+                $db = 'mysql';
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</body>
-<footer class="footer">
-    <div class="container" id="footer">
-        <small>
-            <center>
-                Copyright © 2017 QuantBase Technologies
-            </center>
-        </small>
-    </div>
-</footer>
+                $con = new mysqli('localhost', $user, $pass, $db) or die("Unable to connect");
 
-</html>
+                if(mysqli_connect_errno())
+                {
+                echo "Error 69, failed to connect to database";
+                }
+                if (empty($inputName) || empty($inputPass)){
+                   // echo "<center><font color='red'><i>Please fill out all fields</i></font> <br /></center>";
+                }
+                else
+                {
+                    $userName = $inputName;
+                    $statement = $con->prepare("select * FROM Users where name=?");
+                    $statement->bind_param('s',$userName);
+                    $statement->execute();
+                    $statement->store_result();
+                    $statement->bind_result($id,$name,$email,$password);
+                    if($statement->num_rows>0) { 
+                        while($statement->fetch()){
+                            if( $inputPass == $password)
+                            {
+                                echo "You are logged in!";
+                            }
+                            else
+                            {
+                                echo "Wrong information input.";
+                            }
+                        }
+                    } 
+                    else
+                    {
+                        echo "User not found. Please create an account.";
+                    }
+                }
+             $con->close();
+
+        ?>
--- signup.php
+++ signup.php
@@ -1,155 +1,58 @@
-<?php 
-session_start();
+<?php
+	$postdata = file_get_contents("php://input");
+    $request = json_decode($postdata);
+	
+    $inputName = $request->username;
+    $inputEmail = $request->email;
+    $inputPass = $request->password;
 ?>
-
-<!DOCTYPE html>
-<html>
-
-<head>
-    <link rel="stylesheet" href="index.css" type="text/css">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
-    <title>Quantbase</title>
-</head>
-
-<body>
-    <!-- navigation bar -->
-    <header>
-        <nav class="navbar navbar-inverse navbar-fixed-top">
-            <div class="container">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"
-                        aria-expanded="false">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="index.php">QuantBase</a>
-                </div>
-                <!-- elements in navbar to collapse into button -->
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <ul class="nav navbar-nav">
-                        <li>
-                            <a href="exchange.php">Exchange</a>
-                        </li>
-                        <li>
-                            <a href="portfolio.php">Portfolio</a>
-                        </li>
-                    </ul>
-                    <ul class="nav navbar-nav navbar-right">
-                        <li>
-                            <a href="signup.php">
-                                <span class="glyphicon glyphicon-user"></span> Sign Up</a>
-                        </li>
-                        <li>
-                            <a href="login.php">
-                                <span class="glyphicon glyphicon-log-in"></span> Login</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    </header>
-    <center>
-        <div id="invite-msg">
-        </div>
-    </center>
-
-    <!-- Sign up form -->
-    <div class="container" id="signup-form">
-        <div class="row">
-            <div class="col-md-2">
-            <!-- Form to post user inputs to $_SERVER -->
-            <!-- ans1 = name, ans2 = email, ans3 = submit -->
-            </div>
-            <div class="col-md-8" id="form">
-                <form class="form-group" action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
-                    <label for="name">Name:</label>
-                    <input class="form-control" name="ans1" 
-                    value="<?php if (isset($_POST['ans1'])) echo $_POST['ans1']; ?>">
-                
-                <form class="form-group" action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
-                    <label for="email">Email address:</label>
-                    <input class="form-control" name="ans2" 
-                    value="<?php if (isset($_POST['ans2'])) echo $_POST['ans2']; ?>">
-
-                <form class="form-group" action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
-                    <label for="email">Password:</label>
-                    <input class="form-control" name="ans3" type="password"
-                    value="<?php if (isset($_POST['ans3'])) echo $_POST['ans3']; ?>">
-                    <input type="submit"/>
-                </form>
-            </div>
-        </div>
-        <div class="col-md-2">
-
-        </div>
-    </div>
 
 <?php
-// class definition for user
-class user{
-    public $name;
-    public $email;
-    public $password;
-    public $stocks;
-    public function __construct($name, $email, $password, $stocks){
-        $this->name = $name;
-        $this->email= $email;
-        $this->password = $password;
-        $this->stocks = $stocks;
-    }
-}
-$allUsers = array();
-$feedback = NULL;
-// $bar = new user("Paul", "asdf@asdf", "rafara", array("GOOGL", "MSF"));
-// echo $bar->stocks[0];
 
-// what to do when button is clicked
-if ($_SERVER['REQUEST_METHOD'] == 'POST')
-{
-    //if any of te fields are empty, alert user
-   if (empty($_POST['ans1']) || empty($_POST['ans2']) || empty($_POST['ans3']))
-      echo "<center><font color='red'><i>Please fill out all fields</i></font> <br /></center>";
-   else 
-   {   	//remove user space
-        $ans1 = trim($_POST['ans1']);     
-        $ans2 = trim($_POST['ans2']);
-        $ans3 = trim($_POST['ans3']);
+                $user = 'root';
+                $pass = '';
+                $db = 'mysql';
 
-        $newUser = new user($ans1, $ans2, $ans3, null);
-        $match = 0;
-        //go through each key-value pair and check if there is a match
-        foreach($_SESSION as $key => $value){
-            if($key == $ans2){
-                $match++;
-            }
-        }
-        //if the inputted email has been used, warn user
-        if($match > 0){
-            echo "<center><font color='red'><i>Email has already been used</i></font></center>";
-        }
-        else{
-            //if email has never been used before, make account and add to $_SESSION
-            $_SESSION[$ans2] = $newUser;
-            echo "<center><font color='green'><i>Account has been made!</i></font></center>";
-        }
-   }      
-}
-?>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</body>
-<footer class="footer">
-    <div class="container" id="footer">
-        <small>
-            <center>
-                Copyright © 2017 QuantBase Technologies
-            </center>
-        </small>
-    </div>
-</footer>
+                $con = new mysqli('localhost', $user, $pass, $db) or die("Unable to connect");
 
-</html>
+                if(mysqli_connect_errno())
+                {
+                echo "Error 69, failed to connect to database";
+                }
+                if (empty($inputName) || empty($inputPass)){
+                   // echo "<center><font color='red'><i>Please fill out all fields</i></font> <br /></center>";
+                }
+                else
+                {
+                    $userName = $inputName;
+                    $statement = $con->prepare("select * FROM Users where name=?");
+                    $statement->bind_param('s',$userName);
+                    $statement->execute();
+                    $statement->store_result();
+                    $statement->bind_result($id,$name,$email,$password);
+                    if($statement->num_rows>0) { 
+                        echo "Account already exists.";
+                        exit;
+                    } 
+                    else
+                    {
+                        mysqli_query($con,"INSERT INTO Users(id, name, email, password) VALUES (NULL, '$inputName', '$inputEmail', '$inputPass')");
+                    }
+                $con->close();
+                }
+                    //     while($statement->fetch()){
+                    //         if( $inputPass == $password)
+                    //         {
+                    //             echo "You are logged in!";
+                    //         }
+                    //         else
+                    //         {
+                    //             echo "Wrong information input.";
+                    //         }
+                    //     }
+                    // } 
+                    // else
+                    // {
+                    //     echo "User not found. Please create an account.";
+                    // }
+        ?>
